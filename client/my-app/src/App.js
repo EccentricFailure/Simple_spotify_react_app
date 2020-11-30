4d6120e1cd43cf35aba62ee0201f5a46dc1de153
s8_stiege_j <s8_stiege_j@venta.lv>
2020-12-01 01:08:54 +0200

    idk

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://terriblerealestateagentphotos.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enjoy
         </a>
       </header>
     </div>
