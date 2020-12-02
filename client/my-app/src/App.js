b53ac8b9a32e5dce396bb90ef5e5e0f9e3de75c4
s8_stiege_j <s8_stiege_j@venta.lv>
2020-12-02 21:03:58 +0200

    Added backend api

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,21 +1,42 @@
 //import './App.scss';
-import React from'react';
+import React, { Component } from'react';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Pagetwo from './components/Pagetwo';
 import { Link, Route, Switch } from "react-router-dom";
 import { BreakpointProvider } from 'react-socks';
 
-function App() { 
-  return (
-    <BreakpointProvider>
-      <div className="App">
-        <Navbar />
-        <Route path="/content"><Content /></Route>
-        <Route path="/content2"><Pagetwo /></Route>
-      </div>
-    </BreakpointProvider>
-  );
+class App extends Component{ 
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  
+  render(){
+    return (
+        <BreakpointProvider>
+          <div className="App">
+            <Navbar />
+            
+            <Route path="/content"><Content /></Route>
+            <Route path="/content2"><Pagetwo /></Route>
+          </div>
+        </BreakpointProvider>
+      );
+  }
+ 
 }
 
 export default App;
--- client/my-app/src/components/Content.js
+++ client/my-app/src/components/Content.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './content.scss';
 
-const Content=() => {
-  const data=(
-    <div className="box">
-        <h2>My div content </h2>
-    </div>
-  )
+const data=(
+  <div className="box">
+      <h2>My div content </h2>
+  </div>
+)
 
-return (
-    <main className="content">
-        {data}
-        {data}
-        {data}
-        {data}
-        {data}
-    </main>
-  )
+class Content extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }));
+}
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render(){
+    return (
+      <main className="content">
+        <p className='App-intro'>{this.state.apiResponse}</p>
+          {data}
+          {data}
+          {data}
+          {data}
+          {data}
+      </main>
+    )
+  }
 }
 export default Content;
